29ee185ddd12185e9a0a9d2960ea442e597433dd
Don Gordon <OpNop@users.noreply.github.com>
2020-02-06 19:21:36 -0700

    Empty Projects

---
+++ Chat Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chat_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
