828ae65ad990e240d191d2bcf71fa4bdaa1b6394
Don Gordon <OpNop@users.noreply.github.com>
2020-10-26 03:54:35 -0700

    Setting Discord RPC off by default

--- Chat Client/Properties/Settings.Designer.cs
+++ Chat Client/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace Chat_Client.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool enableDiscord {
             get {
                 return ((bool)(this["enableDiscord"]));
